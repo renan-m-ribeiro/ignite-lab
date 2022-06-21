a3c2d61fdccfe19519993fd0f0554083d89c8265
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-06-21 20:26:23 -0300

    modify App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <h1>Hello World!</h1>
   )
