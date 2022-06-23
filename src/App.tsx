959589ff0e876739576eaa9676fac25079e79fa1
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-06-22 23:52:43 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import { gql, useQuery } from "@apollo/client"
+import { Event } from "./pages/Event"
 
-
-const GET_LESSONS_QUERY = gql`
-
-query {
-  lessons {
-    id
-    title
-  }
-}
-`
-
-interface Lesson {
-  id: string
-  title: string
-}
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <Event />
   )
 }
 
